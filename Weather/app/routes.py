492a3a359d9426c865bd1144b3d727f9bd0b7bbc
ichx <czn16@qq.com>
2018-10-02 15:58:53 +0800

    First Version
    Realized the basic function and interface.

--- Weather/app/routes.py
+++ Weather/app/routes.py
@@ -10,4 +10,4 @@ def index(area='城区'):
 	area_data = [['城区','番禺','从化','增城'],['花都','荔湾','越秀','珠海'],['天河','白云','黄埔','南沙']]
 	predict_data = weather_predict.tomorrow_weather(area)
 	#print(predict_data)
-	return render_template('index.html', area_data=area_data, area=area, detail_data=predict_data['detail'], weather_data = predict_data['weather'], live_index = predict_data['live_index'])
+	return render_template('index.html', area_data=area_data, area=area, detail_data=predict_data['detail'], weather_data=predict_data['weather'], live_index=predict_data['live_index'])
